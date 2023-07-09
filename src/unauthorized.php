3df3cf339a817fca152a7861dd0d9d71f3f6b3a7
Charitha <charitha@mac.socal.rr.com>
2023-07-09 14:21:27 -0700

    3rd commit

---
+++ src/unauthorized.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Unauthorized Access</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 400px;
+            margin: 50px auto;
+            background-color: #fff;
+            border-radius: 5px;
+            padding: 20px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            text-align: center;
+            color: #333;
+        }
+
+        p {
+            text-align: center;
+            margin-top: 15px;
+        }
+
+        a {
+            color: #4caf50;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Unauthorized Access</h1>
+        <p>You are not authorized to access this page.</p>
+        <p>Please <a href="member/registration.php">Signup</a> to continue.</p>
+    </div>
+</body>
+</html>
